eb245620ab97008cf2c1aa0032d659abfe4817c7
MatteoGioioso <gioioso.matteo@gmail.com>
2021-08-09 18:19:39 +0700

    fix dockerhost

--- dockerhost/workers.sh
+++ dockerhost/workers.sh
@@ -6,6 +6,7 @@ virt-install \
   --bridge=br0 \
   --name "${worker_name}" \
   --ram 4096 \
-  --disk path=/var/lib/libvirt/images/"${worker_name}".qcow2,size=50 \
+  --disk path=/var/lib/libvirt/images/"${worker_name}".qcow2,size=150 \
   --vcpus 2 \
-  --cdrom /var/lib/libvirt/isos/ubuntu-20.04.2-live-server-amd64.iso
+  --cdrom /var/lib/libvirt/isos/ubuntu-20.04.2-live-server-amd64.iso \
+  --autostart
