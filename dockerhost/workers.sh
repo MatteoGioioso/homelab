13cd311564c12828d82edcb9d85e8169b7571dc7
MatteoGioioso <gioioso.matteo@gmail.com>
2021-08-09 08:19:08 +0700

    fix vagrant and split playbooks with common playbook add docker host vm

---
+++ dockerhost/workers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+worker_name=docker-host
+
+virt-install \
+  --bridge=br0 \
+  --name "${worker_name}" \
+  --ram 4096 \
+  --disk path=/var/lib/libvirt/images/"${worker_name}".qcow2,size=50 \
+  --vcpus 2 \
+  --cdrom /var/lib/libvirt/isos/ubuntu-20.04.2-live-server-amd64.iso
